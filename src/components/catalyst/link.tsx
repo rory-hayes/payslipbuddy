c2f36a516b94be585c97dbe65cc501d6a214b0e8
Rory <rory@Rorys-MacBook-Air.local>
2026-02-28 21:03:36 +0000

    Fix prerender crash by removing DataInteractive wrapper from Next Link

--- src/components/catalyst/link.tsx
+++ src/components/catalyst/link.tsx
@@ -1,4 +1,3 @@
-import * as Headless from '@headlessui/react'
 import NextLink, { type LinkProps } from 'next/link'
 import React, { forwardRef } from 'react'
 
@@ -6,9 +5,5 @@ export const Link = forwardRef(function Link(
   props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
-  return (
-    <Headless.DataInteractive>
-      <NextLink {...props} ref={ref} />
-    </Headless.DataInteractive>
-  )
+  return <NextLink {...props} ref={ref} />
 })
